5f44eab1270bcda23d104037b51934b0b85b1d0d
Nicolas Gomez Berlin <nicolasgomez3@gmail.com>
2021-08-22 21:59:35 -0300

    Primer_entrega

--- js/login.js
+++ js/login.js
@@ -1,6 +1,21 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+document.addEventListener("DOMContentLoaded", function(e){
 });
+
+function login(){
+    let usuario = document.getElementById("usuario").value;
+    let contrasenia = document.getElementById("contra").value;
+    
+    if (usuario!="" && contrasenia!=""){ 
+        localStorage.setItem("usuario",usuario)
+            location.href="inicio.html";
+    } 
+    else { 
+         alert("Ingrese usuario y contraseña valido."); 
+    } 
+};
+
+
